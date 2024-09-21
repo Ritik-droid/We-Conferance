439fcba19d79450826969ecc396de1ff64502e70
ritik-droid <iritik358@gmail.com>
2024-09-21 06:27:21 +0000

    commit from ubuntu

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,17 @@ import HomePage from "./Components/HomePage/HomePage";
 import Login from "./Components/Login-SignUP/Login";
 import SignUP from "./Components/Login-SignUP/SingUP";
 import MainNavbar from "./Components/Navbar/Navbar";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-         {/* <MainNavbar /> */}
+        <MainNavbar />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/Login" element={<Login/>} />
-          <Route exact path="/SignUP" element={<SignUP/>} />
+          <Route exact path="/Login" element={<Login />} />
+          {/* <Route exact path="/SignUP" element={<SignUP />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
